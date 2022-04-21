6a43bec6a14409015285ca5754e761458e5b980b
Android Studio <devandroid@ort>
2022-04-21 09:01:56 -0300

    started to learn react, components, component props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+class Square extends React.Component {
+
+    constructor(props){
+        // un componente puede tener constructores parametrizados
+        super(props) // porque extiende de React.Component
+        // Un estado en React es, un almacén de datos mutable
+        // de componentes y que además son autónomos. 
+        // O sea, el estado pertenece una clase autónoma que cualquiera pueda
+        // importar y usar en su aplicación.  
+        this.state = {
+            value: null, // al crearse, el estado no tiene ningun valor.
+        };
+    }
+    render() {
+        return (
+            // cada vez que cualquier boton es tocado, consolelogea click.
+            <button className="square" onClick={()=> console.log('click')}>
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        // recibimos un param con un num
+        // todos los componentes Square tienen una prop llamada value, seteada por el param.
+
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
